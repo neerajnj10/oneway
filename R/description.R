e8ddc306ac69ec191d93f293ade51cbd193fed85
Neeraj Kumar <nekumar@mix.wvu.edu>
2015-08-07 22:57:03 -0400

    Create description.R

---
+++ R/description.R
@@ -0,0 +1,13 @@
+#' Blood coagulation times by diet
+#'
+#' Dataset is coming from a study of blood coagulation. data has 24 animals with 4 diets assigned randomly to each
+#'
+#' @format a data frame with 24 rows and two columns:
+#'
+#' \describe{
+#'   \item{coag}{coagulation time in seconds}
+#'   \item{diet}{diet type: A, B, C, or D}
+#' }
+#' @source "Statistics for Experimenters" by G. P. Box, W. G. Hunter and J. S. Hunter, Wiley, 1978
+#'
+"coagulation"
